0b7430dff00e5add48eb89aeb7de1f630246f190
anizari <anizari@sfu.ca>
2020-03-18 12:37:45 -0700

    updates on player

--- src/main/java/org/sfu/MotaGame/Bean/player/Player.java
+++ src/main/java/org/sfu/MotaGame/Bean/player/Player.java
@@ -1,10 +1,38 @@
 package org.sfu.MotaGame.Bean.player;
 
+import java.awt.Color;
 import java.awt.Graphics;
+
+import org.sfu.MotaGame.Bean.ImageData;
 
 public class Player{
 
 	private int x, y;
+	private ImageData imgData;
+	private boolean up, down, left, right;
+	private static final int speed = 2;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void tick() {
+		if(up)
+			y -= speed;
+		if(down)
+			y += speed;
+		if(left)
+			x -= speed;
+		if(right)
+			x += speed;
+	}
+	
+	public void render(Graphics g) {
+		//Temp player
+		g.setColor(Color.red);
+		g.fillRect(x, y, 32, 32);
+	}
 	
 	public int getX() {
 		return x;
@@ -19,13 +47,5 @@ public class Player{
 		this.y = y;
 	}
 	
-	public Player(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public void render(Graphics g) {
-		
-	}
 	
 }
