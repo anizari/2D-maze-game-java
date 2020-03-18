8f4314a5b90afd948c3a0bf82e7893ab0fc43aa7
Chris <chriskassner19@hotmail.com>
2020-03-17 20:11:06 -0700

    changed key fragment sprite back to key

--- src/main/java/org/sfu/MotaGame/Bean/ImageData.java
+++ src/main/java/org/sfu/MotaGame/Bean/ImageData.java
@@ -40,6 +40,7 @@ public class ImageData {
 			imageMap.put(FLOOR, floorImg);
 			imageMap.put(WALL, wallImg);
 			imageMap.put(REWARD, ItemImg.getSubimage(0, 0, 32, 32));
+			//imageMap.put(REWARD, ItemImg.getSubimage(32, 32, 32, 32));
 			imageMap.put(PUNISHMENT, punishmentImg.getSubimage(0, 32, 32, 32));
 			imageMap.put(ENEMY, enemyImg1.getSubimage(0, 32, 32, 32));
 			imageMap.put(EXIT, exitImg.getSubimage(0, 0, 32, 32));
