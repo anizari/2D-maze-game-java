9342c150160503d522d88d27e69c8ce58c4caf0e
Thomas Chiu <thomas_chiu@sfu.ca>
2020-03-18 18:44:37 -0700

    Enemy class update

--- src/main/java/org/sfu/MotaGame/Bean/player/Enemy.java
+++ src/main/java/org/sfu/MotaGame/Bean/player/Enemy.java
@@ -8,11 +8,20 @@ import org.sfu.MotaGame.Bean.ImageData;
 public class Enemy extends Rectangle{
 
 	private int x, y;
+	float distX;
+	float distY;
 	private ImageData imgData = new ImageData();
+	private int velX = 1;
+	private int velY = 1;
 	
 	public Enemy(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void tick() {
+		x += velX;
+		y += velY;
 	}
 	
 	public void render(Graphics g) {
@@ -30,6 +39,14 @@ public class Enemy extends Rectangle{
 	}
 	public void setY(int y) {
 		this.y = y;
+	}
+	
+	public void setVelX(int x) {
+		this.velX = x;
+	}
+	
+	public void setVelY(int y) {
+		this.velY = y;
 	}
 	
 }
