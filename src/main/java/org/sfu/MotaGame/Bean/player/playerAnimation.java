7a7e6c55b6d331f8bd9ccce175c21c86e687072a
Chris <chriskassner19@hotmail.com>
2020-03-18 23:58:51 -0700

    done jdocs

--- src/main/java/org/sfu/MotaGame/Bean/player/playerAnimation.java
+++ src/main/java/org/sfu/MotaGame/Bean/player/playerAnimation.java
@@ -2,11 +2,26 @@ package org.sfu.MotaGame.Bean.player;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * <h1>playerAnimation</h1>
+ * This class deals with player animation
+ * 
+ * @author Christopher Kassner, Alim Nizari, Thomas Chiu, Yuzhuo Ye
+ * @version 1.0
+ * @since 2020-03-18
+ */
 public class playerAnimation {
 	private BufferedImage[] frames;
 	private long timer, lastTime;
 	private int currentFrame, length;
 	
+	/**
+	 * Constructor for player animation
+	 * 
+	 * @param frames This parameter deals with the frames
+	 * @param length This parameter deals with the length
+	 * @return Nothing.
+	 */
 	public playerAnimation(BufferedImage[] frames, int length) {
 		this.frames = frames;
 		this.length = length;
@@ -15,6 +30,11 @@ public class playerAnimation {
 		lastTime = System.currentTimeMillis();
 	}
 	
+	/**
+	 * This method animates the player
+	 * 
+	 * @return Nothing.
+	 */
 	public void animate() {
 		timer += System.currentTimeMillis() - lastTime;
 		lastTime = System.currentTimeMillis();
@@ -27,6 +47,10 @@ public class playerAnimation {
 		}
 	}
 	
+	/**
+	 * Getter for player animation
+	 *  
+	 */
 	public BufferedImage getFrame() {
 		return frames[currentFrame];
 	}
