ea6789a3796591eff1c6b41e10c3301ac9b7def1
Chris <chriskassner19@hotmail.com>
2020-03-18 23:50:28 -0700

    done jdocs

--- src/main/java/org/sfu/MotaGame/Bean/player/Enemy.java
+++ src/main/java/org/sfu/MotaGame/Bean/player/Enemy.java
@@ -5,6 +5,15 @@ import java.awt.Rectangle;
 
 import org.sfu.MotaGame.Bean.ImageData;
 
+/**
+ * <h1>Enemy</h1>
+ * This class renders the enemies and contains all of the relevant
+ * variables and constructors including the distance between the enemies and the player
+ *  
+ * @author Christopher Kassner, Alim Nizari, Thomas Chiu, Yuzhuo Ye
+ * @version 1.0
+ * @since 2020-03-18
+ */
 public class Enemy extends Rectangle{
 
 	private double x, y;
@@ -14,19 +23,27 @@ public class Enemy extends Rectangle{
 	private ImageData imgData = new ImageData();
 	private double velX;
 	private double velY;
-	
+
+	/**
+	 * Constructor for the Enemy class
+	 * 
+	 */
 	public Enemy(int x, int y) {
 		setBounds(x, y, 32, 32);
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Deals with updating the enemy's position every frame.
+	 * 
+	 */
 	public void tick() {
 		x += velX;
 		y += velY;
 	}
 	
-	/*
+	/**
 	 * This method draws the enemies
 	 * 
 	 * @param g This parameter allows us to use graphics.
@@ -36,72 +53,142 @@ public class Enemy extends Rectangle{
 		g.drawImage(imgData.get(3), (int)(Math.round(x)), (int)(Math.round(y)), 32, 32, null);
 	}
 	
+	/**
+	 * Getter for the x coordinate 
+	 * 
+	 */
 	public double getX() {
 		return x;
 	}
+	
+	/**
+	 * Getter for the y coordinate 
+	 * 
+	 */
 	public double getY() {
 		return y;
 	}
+	
+	/**
+	 * Setter for the x coordinate 
+	 * 
+	 */
 	public void setX(double x) {
 		this.x = x;
 	}
+	
+	/**
+	 * Setter for the y coordinate 
+	 * 
+	 */
 	public void setY(double y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Setter for the x coordinate of the velocity 
+	 * 
+	 */
 	public void setVelX(double x) {
 		this.velX = x;
 	}
 	
+	/**
+	 * Setter for the y coordinate of the velocity 
+	 * 
+	 */
 	public void setVelY(double y) {
 		this.velY = y;
 	}
 	
+	/**
+	 * Getter for the x coordinate of the distance
+	 * 
+	 */
 	public double getDistX() {
 		return distX;
 	}
 	
+	/**
+	 * Getter for the y coordinate of the distance
+	 * 
+	 */
 	public double getDistY() {
 		return distY;
 	}
 	
+	/**
+	 * Setter for the x coordinate of the distance
+	 * 
+	 */
 	public void setDistX(double x) {
 		this.distX = x;
 	}
 	
+	/**
+	 * Setter for the y coordinate of the distance
+	 * 
+	 */
 	public void setDistY(double y) {
 		this.distY = y;
 	}
 	
+	/**
+	 * Getter for the distance
+	 * 
+	 */
 	public double getDistance() {
 		return distance;
 	}
 	
+	/**
+	 * Setter for the distance
+	 * 
+	 */
 	public void setDistance(double d) {
 		this.distance = d;
 	}
 	
+	/**
+	 * Getter for the bounds of the rectangle Object
+	 * 
+	 */
 	@Override
 	public Rectangle getBounds() {
 		return new Rectangle((int) x, (int) y, width, height);
 	}
 
+	/**
+	 * Getter for the top bounds of the rectangle Object
+	 * 
+	 */
 	public Rectangle getBoundsTop() {
 		return new Rectangle((int) x + (width / 2) - ((width / 2) / 2), (int) y, (int) width / 2, (int) height / 2);
 	}
 
+	/**
+	 * Getter for the bottom bounds of the rectangle Object
+	 * 
+	 */
 	public Rectangle getBoundsBottom() {
 		return new Rectangle((int) x + (width / 2) - ((width / 2) / 2), (int) y + (height / 2), (int) width / 2, (int) height / 2);
 	}
 
+	/**
+	 * Getter for the right bounds of the rectangle Object
+	 * 
+	 */
 	public Rectangle getBoundsRight() {
 		return new Rectangle((int) x + width - 5, (int) y + 4, (int) 5, (int) height - 8);
 	}
 
+	/**
+	 * Getter for the left bounds of the rectangle Object
+	 * 
+	 */
 	public Rectangle getBoundsLeft() {
 		return new Rectangle((int) x, (int) y + 4, 5, height - 8);
 	}
 
 	
 }
-
