88018197161125c7c1f0c711a814f7df5519eba9
Chris <chriskassner19@hotmail.com>
2020-03-18 23:21:51 -0700

    done jdocs

--- src/main/java/org/sfu/MotaGame/Main.java
+++ src/main/java/org/sfu/MotaGame/Main.java
@@ -2,10 +2,10 @@ package org.sfu.MotaGame;
 
 import javax.swing.*;
 
-/*
+/**
  * <h1>Main</h1>
- * Main creates a new Game, adds a new frame and then starts the game
- * 
+ * This class creates a new Game and a new Jframe.
+ * It then adds the frame to our game and starts the game.
  * 
  * 
  * @author Christopher Kassner, Alim Nizari, Thomas Chiu, John Ye
