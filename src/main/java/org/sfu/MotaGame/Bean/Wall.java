b4b5af6f8e89edf74bd80d0dac0797b193395cf6
Chris <chriskassner19@hotmail.com>
2020-03-18 21:55:23 -0700

    added jdocs

--- src/main/java/org/sfu/MotaGame/Bean/Wall.java
+++ src/main/java/org/sfu/MotaGame/Bean/Wall.java
@@ -7,11 +7,23 @@ import java.awt.Rectangle;
 public class Wall extends Rectangle{
 	private ImageData imgData = new ImageData();
 
-	
+	/*
+	 * This method sets the boundaries for the walls
+	 * 
+	 * @param x This parameter represents the first coordinate of the object
+	 * @param y This parameter represents the second coordinate of the object
+	 * @return Nothing.
+	 */
 	public Wall(int x, int y) {
 		setBounds(x, y, 32, 32);
 	}
 	
+	/*
+	 * This method draws the walls
+	 * 
+	 * @param g This parameter allows us to use graphics.
+	 * @return Nothing.
+	 */
 	public void render(Graphics g) {
 		g.drawImage(imgData.get(9), x, y, 32, 32, null);
 	}
