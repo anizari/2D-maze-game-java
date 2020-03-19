38d4ef39bea9a62350faae6983c384a92151fce3
Chris <chriskassner19@hotmail.com>
2020-03-19 00:00:07 -0700

    done jdocs

--- src/main/java/org/sfu/MotaGame/Bean/ImageData.java
+++ src/main/java/org/sfu/MotaGame/Bean/ImageData.java
@@ -9,9 +9,13 @@ import java.io.File;
 
 /**
  * <h1>Image Data</h1>
- * This class gets all of our sprites for the game and places them on the map
+ * This class contains the integer representations of all of our sprites for the game,
+ * gets them and places them on the map, 
+ * as well as dealing with the floor and direction.
  * 
- * 
+ * @author Christopher Kassner, Alim Nizari, Thomas Chiu, Yuzhuo Ye
+ * @version 1.0
+ * @since 2020-03-18
  */
 public class ImageData {
 
@@ -27,11 +31,11 @@ public class ImageData {
 	private static final int EXIT = 2;
 	private static final int EXIT_OPEN = 5;
 	private static final int BONUSREWARD = 4;
-	public static BufferedImage[] heroUp, heroDown, heroLeft, heroRight;
-	public static BufferedImage hero;
+	private static BufferedImage[] heroUp, heroDown, heroLeft, heroRight;
+	
 	/**
-	 * This method contains all of the data for our images, 
-	 * it gets the sprites and puts them on the map
+	 * This method contains all of the data for our images, gets the sprites
+	 * and puts them on the map
 	 * 
 	 * @return Nothing.
 	 */
@@ -46,7 +50,7 @@ public class ImageData {
 			BufferedImage punishmentImg = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/trap.png"));
 			BufferedImage enemyImg1 = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/enemy02.png"));
 			BufferedImage exitImg = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/door.png"));
-			BufferedImage bonusrewardImg = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/item1.png"));
+			BufferedImage bonusrewardImg = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/item2.png"));
 			BufferedImage heroImg = ImageIO.read(new File(System.getProperty("user.dir") + "/sprite/hero.png"));
 			
 			imageMap.put(FLOOR, floorImg);
@@ -56,9 +60,7 @@ public class ImageData {
 			imageMap.put(ENEMY, enemyImg1.getSubimage(0, 32, 32, 32));
 			imageMap.put(EXIT, exitImg.getSubimage(0, 0, 32, 32));
 			imageMap.put(EXIT_OPEN, exitImg.getSubimage(0, 64, 32, 32));
-			imageMap.put(BONUSREWARD, bonusrewardImg.getSubimage(32, 32, 32, 32));
-			
-			hero = heroImg.getSubimage(0, 32, 32, 32);
+			imageMap.put(BONUSREWARD, bonusrewardImg.getSubimage(0, 32, 32, 32));
 			
 			heroUp = new BufferedImage[4];
 			heroUp[0] = heroImg.getSubimage(0, 0, 32, 32);
@@ -95,11 +97,11 @@ public class ImageData {
 		}
 	}
 	
-	/**
-	 * This method gets the image from the image map
+	/**This method gets the floor of our game
 	 * 
-	 * @param i This parameter is the index of the image
-	 * @return 
+	 * @param i This parameter represents an integer used for the player (1)
+	 * @return imageMap.get(FLOOR) This returns the floor if it's the hero
+	 * @return imageMap.get(i) This returns the imageMap.
 	 */
 	public BufferedImage get(int i) {
 		if ( i == 1) {
@@ -109,4 +111,13 @@ public class ImageData {
 		return imageMap.get(i);
 	}
 	
+	/**This method gets the direction of the player
+	 * 
+	 * @param direction This parameter represents the player's direction
+	 * @return heroImageMap.get(direction) This returns the player's direction
+	 */
+	public BufferedImage getHeroImg(dir direction) {
+		return heroImageMap.get(direction);
+	}
 }
+
